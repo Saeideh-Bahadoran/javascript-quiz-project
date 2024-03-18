3186c2ea50852e32ae1948b2f96f3796352afe38
Saeideh Bahadoran <saeideh.bahadoran@gmail.com>
2024-03-18 15:59:54 +0100

    The-First-Day-Commit

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,43 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
-
-    // 2. getQuestion()
     
-    // 3. moveToNextQuestion()
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining= timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
 
-    // 4. shuffleQuestions()
+     getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+     }
+    
+     moveToNextQuestion(){
+        this.currentQuestionIndex ++;
+     }
 
-    // 5. checkAnswer(answer)
+    shuffleQuestions(){
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.questions[i];
+            this.questions[i] = this.questions[j];
+            this.questions[j] = temp;
+          }
+    }
 
-    // 6. hasEnded()
+    checkAnswer(answer){
+        if(answer === this.questions[this.currentQuestionIndex].answer){
+            this.correctAnswers ++;
+        }
+    }
+
+    hasEnded(){
+        if(this.currentQuestionIndex < this.questions.length){
+            return false;
+        }
+        else if (this.currentQuestionIndex === this.questions.length){
+            return true;
+        }
+    }
+
 }
